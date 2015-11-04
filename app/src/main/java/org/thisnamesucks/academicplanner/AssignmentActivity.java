ffc127eda9672835e31bf882d209f97661b5e95e
Carlos Downie <carlos@bpwebdesign.com>
2015-11-03 16:51:49 -0800

    Updated master to have basic required activities.

--- app/src/main/java/org/thisnamesucks/academicplanner/AssignmentActivity.java
+++ app/src/main/java/org/thisnamesucks/academicplanner/AssignmentActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class AssignmentActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/org/thisnamesucks/academicplanner/AssignmentModel.java
@@ -0,0 +1,14 @@
+package org.thisnamesucks.academicplanner;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Carlos on 11/03/2015.
+ */
+public class AssignmentModel {
+
+    @SerializedName("name")
+    public String name;
+    @SerializedName("classname")
+    public String classname;
+}
