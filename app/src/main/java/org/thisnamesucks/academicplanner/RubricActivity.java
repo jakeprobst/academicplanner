07ccb607b9e3d83903d4e31596efbc24c28f7ce7
Jake Probst <jake.probst@gmail.com>
2015-11-17 21:44:50 -0800

    init files

---
+++ app/src/main/java/org/thisnamesucks/academicplanner/RubricActivity.java
@@ -0,0 +1,13 @@
+package org.thisnamesucks.academicplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RubricActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rubric);
+    }
+}
---
+++ app/src/main/java/org/thisnamesucks/academicplanner/RubricModel.java
@@ -0,0 +1,7 @@
+package org.thisnamesucks.academicplanner;
+
+/**
+ * Created by jake on 11/17/15.
+ */
+public class RubricModel {
+}
