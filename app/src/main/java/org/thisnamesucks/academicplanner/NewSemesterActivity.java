8dcc2a2b45ce9b98190e1c6eb8624845ae2400dc
toogymonster <lopezh_a@yahoo.com>
2015-11-23 21:29:36 -0800

    auto-complete lacks file capability

--- app/src/main/java/org/thisnamesucks/academicplanner/NewSemesterActivity.java
+++ app/src/main/java/org/thisnamesucks/academicplanner/NewSemesterActivity.java
@@ -1,5 +1,6 @@
 package org.thisnamesucks.academicplanner;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class NewSemesterActivity extends AppCompatActivity {
@@ -28,6 +31,29 @@ public class NewSemesterActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        Button start_btn = (Button) findViewById(R.id.startdate_btn);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+
+        Button end_btn = (Button) findViewById(R.id.enddate_btn);
+        end_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+
+        // Get a reference to the AutoCompleteTextView in the layout
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_country);
+// Get the string array
+        String[] schools = getResources().getStringArray(R.array.schools_array);
+// Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> schoolsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, schools);
+        textView.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
