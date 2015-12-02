4fd56ad27c1a2c3493d6c9db1efeff0fc9d11042
Jake Probst <jake.probst@gmail.com>
2015-12-01 19:15:33 -0800

    actually list things out properly in the navbar

--- app/src/main/java/org/thisnamesucks/academicplanner/NavigationActivity.java
+++ app/src/main/java/org/thisnamesucks/academicplanner/NavigationActivity.java
@@ -1,8 +1,11 @@
 package org.thisnamesucks.academicplanner;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +16,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -43,21 +50,41 @@ public class NavigationActivity extends AppCompatActivity
         //navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void fillToDoList() {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.navbar_layout);
+
+        ArrayList<ClassModel> classes = ClassDataManager.getClassesByIDs(SemesterDataManager.getCurrentSemester().getClasses());
+
+        for(ClassModel c: classes) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View cView = inflater.inflate(R.layout.navbar_class, null);
+
+            TextView text = (TextView) cView.findViewById(R.id.navbar_classname);
+            text.setText(c.getName());
+
+            ListView assignments = (ListView) cView.findViewById(R.id.navbar_assignments);
+            NavDrawerAssignmentAdapter asAdapter = new NavDrawerAssignmentAdapter(this, c.getId());
+            assignments.setAdapter(asAdapter);
+
+            layout.addView(cView);
+        }
+    }
+
     @Override
     public void setContentView(int layoutId) {
 
         fullLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_navigation, null);
         frameLayout = (FrameLayout) fullLayout.findViewById(R.id.navbarcontent);
 
-        //getLayoutInflater().inflate(layoutId, frameLayout, true);
-
         toolbar = (Toolbar) getLayoutInflater().inflate(layoutId, frameLayout, true).findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         super.setContentView(fullLayout);
 
-        ListView dueList = (ListView) findViewById(R.id.navbar_due);
+        /*ListView dueList = (ListView) findViewById(R.id.navbar_due);
         classAdapter = new NavDrawerClassAdapter(this);
-        dueList.setAdapter(classAdapter);
+        dueList.setAdapter(classAdapter);*/
+        fillToDoList();
+
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(
