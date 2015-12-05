620006f616f678c40d9eb18cdc47a8d0ff93759f
toogymonster <lopezh_a@yahoo.com>
2015-12-04 22:51:13 -0800

    -semester list activity made

--- app/src/main/java/org/thisnamesucks/academicplanner/SemesterModel.java
+++ app/src/main/java/org/thisnamesucks/academicplanner/SemesterModel.java
@@ -7,12 +7,24 @@ import java.util.ArrayList;
 
 /**
  * Created by jake on 10/29/15.
+ * edited 12-04-15
  */
-public class SemesterModel {
+public class SemesterModel
+{
     @SerializedName("id")
     private int id;
+
     @SerializedName("name")
     private String name;
+    @SerializedName("season")
+    private String season;
+    @SerializedName("school")
+    private String school;
+    @SerializedName("startdate")
+    private String startdate;
+    @SerializedName("enddate")
+    private String enddate;
+
     @SerializedName("classes")
     private ArrayList<Integer> classes = new ArrayList<>();
 
@@ -36,5 +48,40 @@ public class SemesterModel {
         this.name = name;
     }
 
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(String enddate) {
+        this.enddate = enddate;
+    }
+
+    public void setClasses(ArrayList<Integer> classes) {
+        this.classes = classes;
+    }
 
 }
