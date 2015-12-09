fb08b74a44b6eaeff43ca477912356d5bcafbe8d
toogymonster <lopezh_a@yahoo.com>
2015-12-09 14:51:09 -0800

    added homescreen png and made semester button leads to list of semesters

--- app/src/main/java/org/thisnamesucks/academicplanner/HomeScreenActivity.java
+++ app/src/main/java/org/thisnamesucks/academicplanner/HomeScreenActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -18,11 +20,14 @@ public class HomeScreenActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageView imageView = (ImageView) findViewById(R.id.homescreen_img);
+        imageView.setImageResource(R.drawable.home_icon);
+
         Button semesters_btn = (Button) findViewById(R.id.semesters_btn);
         semesters_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeScreenActivity.this, SemesterActivity.class);
+                Intent intent = new Intent(HomeScreenActivity.this, ScheduleActivity.class);
                 startActivity(intent);
             }
         });
