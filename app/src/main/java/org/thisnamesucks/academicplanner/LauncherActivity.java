ffcc0b8807d32f398e62cca70d70b7e056b8f18c
jakeprobst <jake.probst@gmail.com>
2015-12-10 15:09:22 -0800

    handle not having a semester

--- app/src/main/java/org/thisnamesucks/academicplanner/LauncherActivity.java
+++ app/src/main/java/org/thisnamesucks/academicplanner/LauncherActivity.java
@@ -11,17 +11,21 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Settings.SettingsModel settings = Settings.getInstance(this);
         SemesterDataManager.initialize(this);
         ClassDataManager.initialize(this);
         AssignmentDataManager.initialize(this);
 
         // TODO: actually handle not having a current semester with a redirect to create one
-        if (SemesterDataManager.getCurrentSemester() == null) {
-
+        Log.d("launch", Integer.toString(settings.getSemesters().size()));
+        if (settings.getSemesters() == null || settings.getSemesters().size() == 0) {
+            Intent intent = new Intent(this, NewSemesterActivity.class);
+            startActivity(intent);
         }
-
-        Intent intent = new Intent(this, SemesterActivity.class);
-        intent.putExtra("semesterid", SemesterDataManager.getCurrentSemester().getId());
-        startActivity(intent);
+        else {
+            Intent intent = new Intent(this, SemesterActivity.class);
+            intent.putExtra("semesterid", SemesterDataManager.getCurrentSemester().getId());
+            startActivity(intent);
+        }
     }
 }
